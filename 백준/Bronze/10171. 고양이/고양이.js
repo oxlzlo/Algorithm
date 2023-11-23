8705c2e739fb64b5f62c99561b391be274a980bc
김여진 <140046183+oxlzlo@users.noreply.github.com>
2023-11-23 16:35:15 +0900

    [Bronze V] Title: 고양이, Time: 136 ms, Memory: 9420 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,18 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+
+    console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`)
+    
+    process.exit();
+});
