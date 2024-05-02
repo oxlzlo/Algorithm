04ae8423806b2426d779ba376c0b09c35b69e131
김여진 <140046183+oxlzlo@users.noreply.github.com>
2024-05-02 10:58:48 +0900

    [level 0] Title: 가위 바위 보, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -4,9 +4,11 @@ function solution(rsp) {
     for (let i=0; i<rsp.length; i++) {
         if (rsp[i] == 2) {
             answer = answer + '0'
-        } else if (rsp[i] == 0) {
+        } 
+        if (rsp[i] == 0) {
             answer = answer + '5'
-        } else {
+        } 
+        if (rsp[i] == 5) {
             answer = answer + '2'
         }
     }
